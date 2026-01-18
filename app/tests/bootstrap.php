8e8d69474298490744e9e282294e909f428f16cb
gigacache <dmcgillz@icloud.com>
2026-01-18 17:22:04 +0000

    debug ci/cd

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -34,6 +34,8 @@ if (empty($_SERVER['HTTP_HOST']) && !Configure::read('App.fullBaseUrl')) {
     Configure::write('App.fullBaseUrl', 'http://localhost');
 }
 
+Configure::write('debug', true);
+
 // Fixate now to avoid one-second-leap-issues
 Chronos::setTestNow(Chronos::now());
 
